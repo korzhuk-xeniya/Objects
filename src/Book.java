984fb488c797b6b01aa6bb50c1907a3026d6a38a
Ксения <korzhuk.xeniya@yandex.ru>
2023-06-11 20:57:14 +0300

    task done

--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,17 @@
 import ObjectsAndClasses.Author;
 
+import java.util.Objects;
+
 public class Book {
-   private String title;
-   private Author author;
-   private int yearOfPublicatoin;
-   public Book (String title, Author author, int yearOfPublicatoin) {
-       this.title = title;
-       this.author = author;
-       this.yearOfPublicatoin = yearOfPublicatoin;
-   }
+    private String title;
+    private Author author;
+    private int yearOfPublication;
+
+    public Book(String title, Author author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
 
     public Author getAuthor() {
         return author;
@@ -18,10 +21,34 @@ public class Book {
         return title;
     }
 
-    public int getYearOfPublicatoin() {
-        return this.yearOfPublicatoin;
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
     }
-    public void setYearOfPublicatoin (int year) {
-       this.yearOfPublicatoin = year;
+
+    public void setYearOfPublication(int year) {
+        this.yearOfPublication = year;
+    }
+
+    @Override
+    public String toString() {
+        return author.toString() + " Название: " + title +
+                " Год публикации: " + yearOfPublication;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, yearOfPublication);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(yearOfPublication, book.yearOfPublication);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@ import ObjectsAndClasses.Author;
 
 public class Main {
     public static void main(String[] args) {
-        Author lermontov = new Author("Mikhail" , "Lermontov");
+        Author lermontov = new Author("Mikhail", "Lermontov");
         Book aHeroOfOurTime = new Book("A Hero of Our Time", lermontov, 1840);
         Author dostoyevsky = new Author("Fedor", "Dostoyevsky");
         Book crimeAndPunishment = new Book("Crime and Punishment", dostoyevsky, 1866);
-        aHeroOfOurTime.setYearOfPublicatoin(1841);
-        System.out.println("Автор: " + aHeroOfOurTime.getAuthor().getName() + " " + aHeroOfOurTime.getAuthor().getLastName() + " Название: " + aHeroOfOurTime.getTitle() +
-                " Год публикации: " + aHeroOfOurTime.getYearOfPublicatoin());
-        System.out.println("Автор: " + crimeAndPunishment.getAuthor().getName() + " " + crimeAndPunishment.getAuthor().getLastName() + " Название: " + crimeAndPunishment.getTitle() +
-                " Год публикации: " + crimeAndPunishment.getYearOfPublicatoin());
+        aHeroOfOurTime.setYearOfPublication(1841);
+        System.out.println(aHeroOfOurTime.toString());
+        System.out.println(crimeAndPunishment.toString());
+        Book testBook = new Book("Crime and Punishment", dostoyevsky, 1866);
+        if (testBook.getAuthor().hashCode() != crimeAndPunishment.getAuthor().hashCode()) {
+            System.out.println("Книги разные");
+        } else {
+            if (testBook.getAuthor().equals(crimeAndPunishment.getAuthor())) {
+                System.out.println("Автор тот же");
+                if (testBook.hashCode() != crimeAndPunishment.hashCode()) {
+                    System.out.println("Книги разные");
+                } else {
+                    if (testBook.equals(crimeAndPunishment)) {
+                        System.out.println("Книги одинаковые");
+                    }
+                }
+
+            }
+        }
     }
 }
